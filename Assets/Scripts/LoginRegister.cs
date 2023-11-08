4f3688ed1ce0075c03732eb271f0b15300429785
Sachiterasu <37966000+SachihoWolfy@users.noreply.github.com>
2023-11-08 10:36:34 -0700

    Auth build

--- Assets/Scripts/LoginRegister.cs
+++ Assets/Scripts/LoginRegister.cs
@@ -3,29 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
+
 public class LoginRegister : MonoBehaviour
 {
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+    public TextMeshProUGUI displayText;
+    public UnityEvent onLoggedIn;
 
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector]
+    public string playFabId;
+
+    public static LoginRegister instance;
+    void Awake() { instance = this; }
+    public void OnRegister()
     {
-        string username = "TestUser";
-        string password = "password1";
+
         RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest
         {
-            Username = username,
-            DisplayName = username,
-            Password = password,
+            Username = usernameInput.text,
+            DisplayName = usernameInput.text,
+            Password = passwordInput.text,
             RequireBothUsernameAndEmail = false
         };
-        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,result =>
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            registerRequest,
+            result => { SetDisplayText(result.PlayFabId, Color.green); },
+            error => { SetDisplayText(error.ErrorMessage, Color.red); }
+        );
+    }
+    public void OnLoginButton()
+    {
+        LoginWithPlayFabRequest loginRequest = new LoginWithPlayFabRequest
         {
-             Debug.Log(result.PlayFabId);
-        },
-        error =>
-        {
-             Debug.Log(error.ErrorMessage);
-        });
+            Username = usernameInput.text,
+            Password = passwordInput.text
+        };
+        PlayFabClientAPI.LoginWithPlayFab
+        (
+            loginRequest,
+            result =>
+            {
+                playFabId = result.PlayFabId;
+                SetDisplayText("Logged in as: " + result.PlayFabId, Color.green);
+
+                if (onLoggedIn != null)
+                    onLoggedIn.Invoke();
+            },
+            error => SetDisplayText(error.ErrorMessage, Color.red)
+        );
+    }
+    void SetDisplayText(string text, Color color)
+    {
+        displayText.text = text;
+        displayText.color = color;
     }
 
 }
